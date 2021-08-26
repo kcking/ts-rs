22eb4706dc2bb41f7e160a1d888151d167619748
Kevin King <4kevinking@gmail.com>
2021-08-26 15:30:44 -0700

    DateTime<UTC> support?

--- ts-rs/src/lib.rs
+++ ts-rs/src/lib.rs
@@ -27,6 +27,8 @@ use std::{
 
 use uuid::Uuid;
 
+use chrono::{DateTime, Utc};
+
 use std::any::TypeId;
 pub use ts_rs_macros::TS;
 
@@ -250,7 +252,7 @@ impl_primitives! {
     u8, i8, u16, i16, u32, i32, u64, i64, f32, f64, usize, isize => "number",
     u128, i128 => "bigint",
     bool => "boolean",
-    String, &'static str, Uuid => "string",
+    String, &'static str, Uuid, DateTime<Utc> => "string",
     () => "null"
 }
 impl_tuples!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10);
