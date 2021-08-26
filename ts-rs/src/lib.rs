9261230c9f00960110d94a1db54560846ab0a215
Kevin King <4kevinking@gmail.com>
2021-08-26 15:41:43 -0700

    again

--- ts-rs/src/lib.rs
+++ ts-rs/src/lib.rs
@@ -253,7 +253,7 @@ impl_primitives! {
     u128, i128 => "bigint",
     bool => "boolean",
     String, &'static str, Uuid => "string",
-    () => "null"
+    Utc, () => "null"
 }
 impl_tuples!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10);
 impl_proxy!(impl<T: TS> TS for Box<T>);
